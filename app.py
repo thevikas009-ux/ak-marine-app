9bea12cd3eefe1b37c40714a3a0609089075e258
thevikas009-ux <thevikas009@gmail.com>
2026-06-03 16:53:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -873,6 +873,6 @@ href="https://unpkg.com/leaflet/dist/leaflet.css"/>
 # Is code ki madad se aapka HTML pure webpage par automatic render (bann) jayega
 components.html(
     html_content,
-    height=1000,
+    height=5000,
     scrolling=False
 )
